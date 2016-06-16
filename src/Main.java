cac56bff2e9097d052ed278c3295f3cf15435a02
Minhawn <johnwon.us@gmail.com>
2016-06-16 12:42:02 -0400

    Added a Sign-in and Sign-up button with response

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 
 public class Main extends Application{
@@ -25,7 +28,9 @@ public class Main extends Application{
     Label pw = new Label("Password: ");
     PasswordField pwBox = new PasswordField();
     Button signin = new Button("Sign in");
-    HBox hbBtn = new HBox(10);
+    Button signup = new Button("Sign up");
+    HBox hbBtn = new HBox(4);
+    Text actiontarget = new Text();
 
     @Override
     public void start(Stage primaryStage)
@@ -38,7 +43,6 @@ public class Main extends Application{
         grid.setVgap(10);
         grid.setPadding(new Insets(50, 50, 50, 50));
 
-
         Scene scene = new Scene(grid, 400, 300);
         primaryStage.setScene(scene);
 
@@ -49,16 +53,36 @@ public class Main extends Application{
         grid.add(userTextField, 1, 1);
         grid.add(pw, 0, 2);
         grid.add(pwBox, 1, 2);
+        grid.add(actiontarget, 1, 4);
+        grid.add(hbBtn, 1, 3);
 
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(signup);
         hbBtn.getChildren().add(signin);
 
-        grid.add(hbBtn, 1, 4);
+        signin.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent e)
+            {
+                actiontarget.setFill(Color.RED);
+                actiontarget.setText("Sign in Pressed");
+            }
+        });
 
-
+        signup.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent e)
+            {
+                actiontarget.setFill(Color.CADETBLUE);
+                actiontarget.setText("Sign up Pressed");
+            }
+        });
 
         primaryStage.show();
     }
+
 
 
 
